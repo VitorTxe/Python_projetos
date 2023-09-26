c453fbf244139bae0a5b2b1f0de12f08b8f8d3cb
VitorTxe <143734032+VitorTxe@users.noreply.github.com>
2023-09-26 14:18:24 -0300

    Update IGN_analise.py

--- IGN_analise.py
+++ IGN_analise.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Carrega todas as colunas e linhas da tabela 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
